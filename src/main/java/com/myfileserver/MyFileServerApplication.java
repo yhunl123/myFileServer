575acba07889baff83fc5b84dab64a680ad8e174
이용일 <yhunl123@g-oduk.com>
2022-11-24 10:05:30 +0900

    프로젝트 생성

---
+++ src/main/java/com/myfileserver/MyFileServerApplication.java
@@ -0,0 +1,13 @@
+package com.myfileserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFileServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFileServerApplication.class, args);
+    }
+
+}
